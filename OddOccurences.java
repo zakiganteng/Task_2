347385e21e0ee2a8a700e8a441b369ab9b675233
adf-telkomuniv <adf.asd.telkomuniversity@gmail.com>
2016-01-19 16:29:09 +0700

    Create OddOccurences.java

---
+++ OddOccurences.java
@@ -0,0 +1,16 @@
+public class OddOccurences{
+  
+  public int checkOddOccurences( int[] A ){
+    // write your solution here
+  }
+  
+  public static void main(String[] args){
+    OddOccurences odd = new OddOccurences();
+    
+    int[] A = {};
+    
+    int result = odd.checkOddOccurences( A );
+    
+  }
+  
+}
